4b02915457d4dab6992dc64746d5d67dc12087fe
Matheus <matheussilveirarocha.sc@gmail.com>
2022-11-29 15:20:04 -0300

    Linha de teste retirada

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -16,7 +16,6 @@ export default class Modal {
 
   // Adiciona o evento de toggle ao modal
   eventToggleModal(event) {
-    console.log(this)
     event.preventDefault();
     this.toggleModal();
   }
